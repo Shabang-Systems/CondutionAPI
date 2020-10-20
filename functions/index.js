e23b3c1b6752f98642cb5f1f99004be94ba399f4
zbuster05 <zakary.saya@gmail.com>
2020-10-20 09:20:27 -0700

    Deleted the garbage

--- functions/index.js
+++ functions/index.js
@@ -20,38 +20,6 @@ const E = require('./Engine/CondutionEngine');
 E.start({admin}, "fb-admin");
 E.use("fb-admin");
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Cloud Firestore under the path /messages/:documentId/original
-/*exports.addTask = functions.https.onRequest(async (req, res) => {*/
-    //// Grab the text parameter.
-    //// Send back a message that we've succesfully written the message
-    
-    //if (!req.query.token) {
-        //res.json({error: "Missing auth token"})
-        //return;
-    //};
-    //admin.auth().verifyIdToken(req.query.token)
-      //.then(function(decodedToken) {
-          //let uid = decodedToken.uid;
-          //let ntObject = {
-              //desc: "",
-              //isFlagged: false,
-              //isFloating: false,
-              //isComplete: false,
-              //project: "",
-              //tags: [],
-              //timezone: "America/Los_Angeles",
-              //repeat: {rule: "none"},
-              //name: "A nananana papapa",
-          //};
-          //E.db.newTask(uid, ntObject);
-          //res.json({result: uid});
-      //}).catch(function(error) {
-          //console.log(error);
-          //res.json({error: error});
-      //});
-//});
-
 exports.addTask = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     // Send back a message that we've succesfully written the message
